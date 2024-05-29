af4f9fdc69e71a70a51221edc3d7bedd09d29552
Juan Manuel Anton <juanma.03.17.07@gmail.com>
2024-05-28 23:20:38 -0500

    vue router prod

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router';
 import routes from './routes';
 
 const router = createRouter({
   // history: createWebHistory(import.meta.env.BASE_URL),
   // history: createWebHistory('/portfolio/'),
-  history: createWebHistory(process.env.NODE_ENV === 'production' ? '/portfolio/' : '/'),
+  history: createWebHashHistory(process.env.NODE_ENV === 'production' ? '/portfolio/' : '/'),
   linkExactActiveClass: 'active',
   routes: routes,
 })
