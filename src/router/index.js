e5b37b329b2bdc81a1078a5c789c5d2dc52a499c
Juan Manuel Anton <juanma.03.17.07@gmail.com>
2024-05-28 23:12:59 -0500

    vue router prod

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import routes from './routes';
 
 const router = createRouter({
   // history: createWebHistory(import.meta.env.BASE_URL),
-  history: createWebHistory(process.env.NODE_ENV === 'production' ? '/portfolio/' : '/'),
+  history: createWebHistory('/portfolio/'),
   linkExactActiveClass: 'active',
   routes: routes,
 })
