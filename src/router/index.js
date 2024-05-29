56f014207cab272191d8d370ddb7224df78b22bf
Juan Manuel Anton <juanma.03.17.07@gmail.com>
2024-05-28 22:50:43 -0500

    vue router prod

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   linkExactActiveClass: 'active',
   routes: routes,
-  base: 'portfolio',
+  base: '/portfolio/',
 })
 
 export default router
