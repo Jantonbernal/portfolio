d3e85da9f7ef1bf0f9e4fdab066d0d89418bad75
Juan Manuel Anton <juanma.03.17.07@gmail.com>
2024-05-28 21:02:55 -0500

    vue router fixed

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,8 @@ import routes from './routes';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   linkExactActiveClass: 'active',
-  routes: routes
+  routes: routes,
+  base: process.env.NODE_ENV === 'production' ? 'portfolio' : '/',
 })
 
 export default router
