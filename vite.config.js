2745e58f6678c9b1cb52e0dc650d59fdd82484c0
Juan Manuel Anton <juanma.03.17.07@gmail.com>
2024-05-29 23:28:24 -0500

    sitemal.xml

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import sitemap from 'vite-plugin-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,21 @@ export default defineConfig({
   plugins: [
     vue(),
     VueDevTools(),
+    sitemap({
+      base: 'https://jantonbernal.github.io/portfolio/#/',
+      fileName: 'sitemap.xml',
+      exclude: ['/404'], // Rutas que deseas excluir del sitemap
+      routes: [
+        // Lista las rutas de tu aplicación aquí
+        { url: '/', changefreq: 'weekly', priority: 1.0 },
+        { url: '/experience', changefreq: 'weekly', priority: 0.8 },
+        { url: '/projects', changefreq: 'weekly', priority: 0.8 },
+        { url: '/courses', changefreq: 'weekly', priority: 0.8 },
+        { url: '/systems', changefreq: 'weekly', priority: 0.8 },
+        { url: '/contact', changefreq: 'weekly', priority: 0.8 },
+        // Agrega más rutas según sea necesario
+      ],
+    }),
   ],
   resolve: {
     alias: {
